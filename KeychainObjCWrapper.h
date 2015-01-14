09072a9498dc04934da19e2a0138d57b520ed118
Paul Ardeleanu - Pro2 <pa@h24.io>
2015-01-14 10:20:04 +0000

    removed copyright notices

--- KeychainObjCWrapper.h
+++ KeychainObjCWrapper.h
@@ -3,7 +3,6 @@
 //  PAKeychainService
 //
 //  Created by Andrew Ebling on 02/01/2015.
-//  Copyright (c) 2015 Tenero Mobile Limited. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- PAKeychainService.swift
+++ PAKeychainService.swift
@@ -25,33 +25,33 @@ private let _sharedService = PAKeychainService()
 
 
 class PAKeychainService: NSObject {
-    
+
     class func sharedService() -> PAKeychainService {
         return _sharedService
     }
-    
-    
+
+
     func saveContentsForKey(value: String, key: String) {
-        
+
         let dataFromString = value.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
-        
+
         let keychainQuery = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, serviceString(),      key,                  dataFromString],
             forKeys: [kSecClassValue,                kSecAttrServiceValue, kSecAttrAccountValue, kSecValueDataValue])
-        
+
         SecItemDelete(keychainQuery as CFDictionaryRef)
         SecItemAdd(keychainQuery as CFDictionaryRef, nil)
     }
-    
-    
+
+
     func getContentsOfKey(key: String) -> String? {
-        
+
         let keychainQuery = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, serviceString(),      key,                  kCFBooleanTrue,      kSecMatchLimitOneValue],
             forKeys: [kSecClassValue,                kSecAttrServiceValue, kSecAttrAccountValue, kSecReturnDataValue, kSecMatchLimitValue])
-        
+
         // we do the read in Objective-C to work around a Swift compiler optimization bug (as of Xcode 6.2 (6C101) which resulted in no data being returned
         return  KeychainObjCWrapper.keychainValueForDictionary(keychainQuery)
     }
-    
+
     private func serviceString() -> String {
         return NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleIdentifier") as String
     }
